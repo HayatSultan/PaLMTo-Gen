c9dcbfdb20b1bbdbdaa54258d305331bee90d2eb
Joey Cherisea <hai.p@northeastern.edu>
2025-06-16 21:17:25 -0700

    Update setup script to be ready for pre-release upload

--- palmto_gen/setup.py
+++ palmto_gen/setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 classifiers = [
-    'Development Status :: 5 - Production/Stable',
+    'Development Status :: 3 - Alpha',
     'Intended Audience :: Education',
     'Operating System :: Microsoft :: Windows :: Windows 10',
     'License :: OSI Approved :: MIT License',
-    'Programming Lnaguage :: Python :: 3'
+    'Programming Language :: Python :: 3'
 ]
 
 setup(
     name='Palmto_gen',
-    version='0.2',
+    version='0.3a1',
     packages=find_packages(),
     include_package_data=True,
     package_data={
@@ -22,7 +22,7 @@ setup(
     author='Hayat Sultan',
     author_email='hayatsul@ualberta.ca',
     license='MIT',
-    classfiers = classifiers,
+    classifiers = classifiers,
     keywords='trajectory generation' 'Probablistic Language Models',
     install_requires= ['geopandas', 'tqdm', 'geopy', 'scipy', 'folium']
 )
