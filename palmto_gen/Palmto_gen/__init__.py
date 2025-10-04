c5ed0b5ad1fcfc01c0457431aa2040fa0cd30603
Joey Cherisea <hai.p@northeastern.edu>
2025-10-04 16:07:21 -0700

    Refactor init method of ConvertToTemporalToken to only check for geometry column

--- palmto_gen/Palmto_gen/__init__.py
+++ palmto_gen/Palmto_gen/__init__.py
@@ -922,32 +922,35 @@ class DisplayTrajs():
         ax.set_ylabel('Latitude')
         ax.set_title('Generated Trajectories')
 
+def convert_to_3d_points(coord_list):
+    """Convert 3D coordinate triplets into Shapely Point objects with time information.
+
+    Args:
+        coord_list(list): coordinate triplets in (lon, lat, time) format.
+    
+    Returns:
+        list: coordinate-converted Shapely points with time metadata.
+    """
+
 class ConvertToTemporalToken:
-    def __init__(self, df, area, cell_size, time_interval_secs):
+    def __init__(self, df, area, cell_size, time_interval_mins=30):
         """Initialize a spatial-temporal tokenization class that maps coord pairs to a 3D space.
 
         Args:
             df(pd.DataFrame): dataframe with a minimum of 'geometry', 'timestamp' columns.
             area(gpd.GeoDataFrame): Shapely polygon delimiting the boundary of a geographical region.
-            cell_size: side length of each cell in a grid
-            time_interval_secs: sampling time interval between two adjacent coord pairs measured in seconds.
+            cell_size: side length of each cell in a grid.
+            time_interval_mins: time interval on z axis.
 
         Raises:
             ValueError: if 'timestamp' column is missing or not in Unix time format
         """
-        required_cols = ['geometry', 'timestamp']
-        missing_cols = [col for col in required_cols if col not in df.columns]
-        if missing_cols:
-            raise ValueError(f"Dataframe must contain cols: {missing_cols}")
-        
-        # Enforce Unix time format on timestamp column
-        if not pd.api.types.is_numeric_dtype(df['timestamp']):
-            raise ValueError(f"'Timestamp' column must be in Unix time format.")
+        assert 'geometry' in df.columns, "Required column 'geometry' is missing."
 
         self.gdf = process_data(df)
         self.area = area
         self.cell_size = cell_size
-        self.time_interval_mins = time_interval_secs
+        self.time_interval_mins = time_interval_mins
 
     
     
